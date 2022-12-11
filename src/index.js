2baee8514ecdd84ef2f579dbc2b20ba823b9b139
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2022-12-11 18:14:37 +0330

    "end navigation"

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// javascript
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visiblity = nav.getAttribute("data-visible");
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
